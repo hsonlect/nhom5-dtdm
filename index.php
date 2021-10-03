e6f98972ee5f275a5da89cd8b2eafde6abed874a
lengon <anhlangon1@gmail.com>
2021-10-03 11:29:11 +0700

    lethienngon 3/10

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
 
 echo "Hello World!";
-echo "<br>Họ và tên: Lê Thiện Ngôn MSSV: B1809157";
+echo "<br><br>Họ và tên: Lê Thiện Ngôn MSSV: B1809157";
 echo "<br>Tên của Table: tofulee    Các trường: name, mssv";
+
 ?>
