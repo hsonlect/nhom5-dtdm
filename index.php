1d00842939e0f321e4674a220a1a682efa59dcd6
phuongx <maiphuongtranthimaiphuong@gmail.com>
2021-10-03 20:19:56 +0700

    Mai Phuong add files

--- index.php
+++ index.php
@@ -6,4 +6,7 @@ echo "<br>Tên của Table: tofulee    Các trường: name, mssv";
 
 echo "<br> <hr> Họ và tên: Nguyễn Thị Mỹ Như, mssv: B1809163";
 echo "<br>Tên của table: nhu_table	Các trường: naem, mssv<hr>";
+
+echo "<br> Ho va ten: Tran Thi Mai Phuong; MSSV: B1809170";
+echo "<br>Ten table:  phuong_table - Cac truong: username,  password<hr>";
 ?>
---
+++ tranthimaiphuong/formnhap.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Nhập thong tin</title>
+<head>
+<body>
+	<h4>Mời nhập thông tin</h4>
+	<form action="insert_data.php" method="post">
+		<table>
+			<tr>
+				<td>Username</td>
+				<td><input type="text" name="username"></td>
+			</tr>
+			<tr>
+				<td>Password</td>
+				<td><input type="text" name="password"></td>
+			</tr>
+			<tr>
+				<td><input type="submit" value="Gửi"></td>
+				<td><input type="reset" value="Hủy"></td>
+			</tr>
+		</table>
+	</form>
+</body>
+</html>
