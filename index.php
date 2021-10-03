64c0d85de37e6d61f986b451e8f42683b357cbed
mynhu2108 <mynhuvh@gmail.com>
2021-10-03 18:36:53 +0700

    My Nhu import

--- index.php
+++ index.php
@@ -4,4 +4,6 @@ echo "Hello World!";
 echo "<br><br>Họ và tên: Lê Thiện Ngôn MSSV: B1809157";
 echo "<br>Tên của Table: tofulee    Các trường: name, mssv";
 
+echo "<br> <hr> Họ và tên: Nguyễn Thị Mỹ Như, mssv: B1809163";
+echo "<br>Tên của table: nhu_table	Các trường: naem, mssv<hr>";
 ?>
