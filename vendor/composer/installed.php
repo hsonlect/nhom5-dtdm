16e472292cafbb73849b9ac245efc343d693bc30
lengon <anhlangon1@gmail.com>
2021-10-04 01:09:21 +0700

    lethienngon

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '020d8991a32931064f1568b58c61426435f8fef1',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '020d8991a32931064f1568b58c61426435f8fef1',
+    ),
+  ),
+);
