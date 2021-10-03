5505842a087db8d91d59a793d5d3375e8571d424
Tam <tam1475369@gmail.com>
2021-10-03 20:30:23 +0700

    cap nhat trang index

--- index.php
+++ index.php
@@ -1,5 +1,37 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
 
-echo "Hello World!";
+<head>
+    <meta charset="UTF-8">
+    <title>Lập trình Web _ CT428</title>
+    <link rel="stylesheet" type="text/css" href="style.css" />
+</head>
 
-?>
+<body>
+
+    <div id="main">
+        <div id="left">
+            <div class="menu_left">
+                <h3>Danh sách các thành viên</h3>
+                <ul>
+                    <li><a href="#">Trương Minh Tâm</a></li>
+                    <li><a href="#">Trần Thị Mai Phương</a></li>
+                    <li><a href="#">Lê Hoàng Sơn</a></li>
+                    <li><a href="#">Nguyễn Thị Mỹ Như</a></li>
+                    <li><a href="#">Lê Thiện Ngôn</a></li>
+                    <li><a href="#">Nguyễn Thị Ngọc Ngà</a></li>
+                    <li><a href="#">Nguyễn Hoàng Nguyện</a></li>
+                    <li><a href="#">Lưu Hoàng Phúc</a></li>
+                    <li><a href="#">Hỗ Trọng Nghĩa</a></li>
+                </ul>
+                <img src="https://lazi.vn/uploads/photo/lazi_5ef073fc3f49e_1592816636_85.jpg" width="300px">
+
+            </div>
+        </div>
+        <div id="right">
+
+        </div>
+    </div>
+</body>
+
+</html>
