1ea06804fdafc6149825d1c1e2599f2392854d58
Tam <tam1475369@gmail.com>
2021-10-03 20:42:28 +0700

    cap nhat lai index lan 3

--- index.php
+++ index.php
@@ -33,18 +33,13 @@
 
         </div>
     </div>
+<br><br>Họ và tên: Lê Thiện Ngôn MSSV: B1809157";
+<br>Tên của Table: tofulee    Các trường: name, mssv";
+
+<br> <hr> Họ và tên: Nguyễn Thị Mỹ Như, mssv: B1809163";
+<br>Tên của table: nhu_table	Các trường: naem, mssv<hr>";
+
+<br> Ho va ten: Tran Thi Mai Phuong; MSSV: B1809170";
+<br>Ten table:  phuong_table - Cac truong: username,  password<hr>
 </body>
-
 </html>
-<?php
-=======
-echo "Hello World!";
-echo "<br><br>Họ và tên: Lê Thiện Ngôn MSSV: B1809157";
-echo "<br>Tên của Table: tofulee    Các trường: name, mssv";
-
-echo "<br> <hr> Họ và tên: Nguyễn Thị Mỹ Như, mssv: B1809163";
-echo "<br>Tên của table: nhu_table	Các trường: naem, mssv<hr>";
-
-echo "<br> Ho va ten: Tran Thi Mai Phuong; MSSV: B1809170";
-echo "<br>Ten table:  phuong_table - Cac truong: username,  password<hr>";
-?>
