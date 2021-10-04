7e6a94ebed4a4da4c635fd45249c6894cf18c308
lengon <anhlangon1@gmail.com>
2021-10-04 11:02:01 +0700

    le thien ngon 4/10

--- lethienngon/index.php
+++ lethienngon/index.php
@@ -1,7 +1,7 @@
 <?php
   echo "<h3>Options</h3>";
-  echo "<a href='setup.php' target='_blank'>Create table</a>";
-  echo "<a href='list.php' target='_blank'>List</a>";
-  echo "<a href='add.php' target='_blank'>Add</a>";
-  echo "<a href='delete.php' target='_blank'>Delete</a>";
+  echo "<br><a href='setup.php' target='_blank' style='text-decoration:none'>Create table</a>";
+  echo "<br><a href='list.php' target='_blank' tyle='text-decoration:none'>List</a>";
+  echo "<br><a href='add.php' target='_blank' tyle='text-decoration:none'>Add</a>";
+  echo "<br><a href='delete.php' target='_blank' tyle='text-decoration:none'>Delete</a>";
 ?>
