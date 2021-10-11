a9595604a826b50795ee71f00942d8e4918a647c
Nguyenb1809158 <hoangnguyen2474@gmail.com>
2021-10-11 15:30:48 +0700

    sua chut

--- nguyenhoangnguyen/createtable.php
+++ nguyenhoangnguyen/createtable.php
@@ -1,6 +1,7 @@
 <?php
 function pg_connection_string_from_database_url() {
-  extract(parse_url("postgres://mbspycvjqdzksv:f1e5253bdc82a019559721281157cfc743f5860c324aa2381690d105754c2463@ec2-44->  return "user=$user password=$pass host=$host dbname=" . substr($path, 1); # <- you may want to add sslmode=require th>}
+  extract(parse_url("postgres://mbspycvjqdzksv:f1e5253bdc82a019559721281157cfc743f5860c324aa2381690d105754c2463@ec2-44-198-196-149.compute-1.amazonaws.com:5432/dsr7457q5l6c4"));
+  return "user=$user password=$pass host=$host dbname=" . substr($path, 1); # <- you may want to add sslmode=require th>}
    $db = pg_connect(pg_connection_string_from_database_url() );
    if(!$db){
       echo "Error : Unable to open database\n";
