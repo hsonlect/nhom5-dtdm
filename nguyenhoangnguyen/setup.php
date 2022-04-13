4ec7ebec887070156c4a39e1ac227019faee05b3
Hoàng-Sơn Lê <82049377+hsonlect@users.noreply.github.com>
2022-04-13 08:19:37 +0700

    remove sensitive data: PostgreSQL URI exposed

--- nguyenhoangnguyen/setup.php
+++ nguyenhoangnguyen/setup.php
@@ -1,6 +1,6 @@
 <?php
 function pg_connection_string_from_database_url() {
-  extract(parse_url("postgres://mbspycvjqdzksv:f1e5253bdc82a019559721281157cfc743f5860c324aa2381690d105754c2463@ec2-44-198-196-149.compute-1.amazonaws.com:5432/dsr7457q5l6c4"));
+  extract(parse_url($_ENV["DATABASE_URL"]));
   return "user=$user password=$pass host=$host dbname=" . substr($path, 1); # <- you may want to add sslmode=require there too
 }
    $db = pg_connect(pg_connection_string_from_database_url() );
